7b8b5fc67a00153012c2ff1c691190a5d299f8b1
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2018-04-20 11:25:34 +0200

    Working Bootstrap 3 Modal with Vanilla JS, No shit added

--- modal.js
+++ modal.js
@@ -5,12 +5,16 @@ class Modal {
         this.elem.addEventListener('click', e => {
             if (e.target.classList.contains('close-modal')) {
                 this.elem.style.display = "none";
+                this.elem.classList.remove('in');
+                this.elem.style.backgroundColor = "rgba(0, 0, 0, 1)";
             }
         })
     }
 
     show() {
         this.elem.style.display = "block";
+        this.elem.classList.add('in');
+        this.elem.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
     }
 
 }

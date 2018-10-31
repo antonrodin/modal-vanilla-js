28c7cd01d741efe688b3594a09d235cebc643b1f
Anton Zekeriev Rodin <antonzekeriev@gmail.com>
2018-10-31 10:52:52 +0300

    Add the close event and example number 3

--- modal.js
+++ modal.js
@@ -2,11 +2,7 @@ class Modal {
 
     constructor(id) {
         this.elem = document.getElementById(id);
-        this.elem.addEventListener('click', e => {
-            if(e.target.classList.contains("close-modal")) {
-                this.hide();
-            }
-        });
+        this.loadHideListeners();
     };
 
     show(callback) {
@@ -16,7 +12,31 @@ class Modal {
 
     hide(callback) {
         this.elem.style.display = "none";
+        this.createCloseEvent();
         if (callback) callback();
+    };
+
+    createCloseEvent() {
+        let event = new Event('AzrModalClose');
+        document.dispatchEvent(event);
+    }
+
+    // Create event listeners for close modal
+    loadHideListeners() {
+        
+        // Hide modal when you press close
+        this.elem.addEventListener('click', e => {
+            if(e.target.classList.contains("close-modal")) {
+                this.hide();
+            }
+        });
+
+        // Also hide modal when you press outside
+        this.elem.addEventListener('click', e => {
+            if(e.target.classList.contains('modal-azr')) {
+                this.hide();
+            }
+        });
     };
 
 }
